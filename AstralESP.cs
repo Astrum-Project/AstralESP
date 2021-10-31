878d3c7f478851e92a7ebab2dc4fd63fddb86715
xMiraiChan <xMiraiChan@protonmail.com>
2021-10-31 02:32:17 -0400

    add fps, ping & fix bugs

--- AstralESP.cs
+++ AstralESP.cs
@@ -8,7 +8,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.XR;
 using VRC.SDKBase;
 
-[assembly: MelonInfo(typeof(Astrum.AstralESP), "AstralESP", "0.1.0", downloadLink: "github.com/Astrum-Project/AstralESP")]
+[assembly: MelonInfo(typeof(Astrum.AstralESP), "AstralESP", "0.2.0", downloadLink: "github.com/Astrum-Project/AstralESP")]
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonColor(ConsoleColor.DarkMagenta)]
 
@@ -22,6 +22,8 @@ namespace Astrum
         public float pickupsMaxDistance = 10000f;
 
         public bool playersEnabled = true;
+        public bool playersFPS = true;
+        public bool playersPing = true;
         public bool playersDistance = true;
         public float playersMaxDistance = 10000f;
 
@@ -45,6 +47,8 @@ namespace Astrum
             category.CreateEntry("Pickups-MaxDistance", 10000f, "Pickups - Max Distance");
 
             category.CreateEntry("Players-Enabled", true, "Pickups - Enabled");
+            category.CreateEntry("Players-FPS", true, "Show - FPS");
+            category.CreateEntry("Players-Ping", true, "Show - Ping");
             category.CreateEntry("Players-Distance", true, "Pickups - Show Distance");
             category.CreateEntry("Players-MaxDistance", 10000f, "Pickups - MaxDistance");
 
@@ -63,6 +67,8 @@ namespace Astrum
             pickupsMaxDistance = category.GetEntry<float>("Pickups-MaxDistance").Value;
 
             playersEnabled = category.GetEntry<bool>("Players-Enabled").Value;
+            playersFPS = category.GetEntry<bool>("Players-FPS").Value;
+            playersPing = category.GetEntry<bool>("Players-Ping").Value;
             playersDistance = category.GetEntry<bool>("Players-Distance").Value;
             playersMaxDistance = category.GetEntry<float>("Players-MaxDistance").Value;
 
@@ -82,6 +88,9 @@ namespace Astrum
             if (index != -1) return;
 
             pickups.Clear();
+
+            OnGUIAction -= DrawPickups;
+            OnGUIAction -= DrawPlayers;
         }
 
         public System.Collections.IEnumerator FindPickups(string name)
@@ -91,18 +100,22 @@ namespace Astrum
             while (scene.GetRootGameObjects().FirstOrDefault(go => go.name.StartsWith("VRCPlayer[Local]")) is null) yield return null;
 
             pickups = UnityEngine.Object.FindObjectsOfType<VRC_Pickup>().Select(f => f.gameObject).ToList();
+
+            OnGUIAction += DrawPickups;
+            OnGUIAction += DrawPlayers;
         }
 
-        public override void OnGUI()
-        {
-            if (pickupsEnabled) DrawPickups();
-            if (playersEnabled) DrawPlayers();
-        }
+        public override void OnGUI() => OnGUIAction();
+        private Action OnGUIAction = new Action(() => { });
 
         private void DrawPickups()
         {
+            if (!pickupsEnabled) return;
+            if (Networking.LocalPlayer is null) return;
+            
             foreach (GameObject pickup in pickups)
             {
+
                 if (pickup is null)
                 {
                     pickups.Remove(pickup);
@@ -126,7 +139,7 @@ namespace Astrum
                 if (pickupsDistance)
                 {
                     builder.Append(" | ");
-                    builder.Append(p.z);
+                    builder.Append(p.z.ToString("0.00"));
                 }
 
                 GUI.Label(new Rect(p.x, Screen.height - p.y, 0, 0), builder.ToString(), style);
@@ -135,20 +148,41 @@ namespace Astrum
 
         private void DrawPlayers()
         {
+            if (!playersEnabled) return;
+            if (VRC.PlayerManager.field_Private_Static_PlayerManager_0?.field_Private_List_1_Player_0 is null) return;
+
             foreach (VRC.Player player in VRC.PlayerManager.field_Private_Static_PlayerManager_0.field_Private_List_1_Player_0)
             {
+                if (player is null) continue;
                 Vector3 p = Camera.main.WorldToScreenPoint(player.transform.position);
 
                 if (p.z < 0 || p.z > playersMaxDistance) continue;
 
                 StringBuilder builder = new StringBuilder();
 
+                if (player.field_Private_VRCPlayerApi_0.isMaster)
+                    builder.Append("<color=orange>");
+
                 builder.Append(player.field_Private_APIUser_0.displayName);
+
+                if (playersFPS)
+                {
+                    builder.Append(" | ");
+                    byte f = player._playerNet.field_Private_Byte_0;
+                    if (f == 0) builder.Append('\u221E');
+                    else builder.Append(1000 / f); 
+                }
+
+                if (playersPing)
+                {
+                    builder.Append(" | ");
+                    builder.Append(player.prop_Int32_0);
+                }
 
                 if (playersDistance)
                 {
                     builder.Append(" | ");
-                    builder.Append(p.z);
+                    builder.Append(p.z.ToString("0.00"));
                 }
 
                 GUI.Label(new Rect(p.x, Screen.height - p.y, 0, 0), builder.ToString(), style);
